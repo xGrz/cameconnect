bb5ea6c011699d22da47f45c7e9e457f026ca5b5
xGrz <grzesiek.byskiniewicz@gmail.com>
2024-12-03 12:59:27 +0100

    Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,33 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import {defineConfig} from 'vite';
+import laravel, {refreshPaths} from 'laravel-vite-plugin';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const host = `${process.env.APP_URL}`
+    .replace('http://', '')
+    .replace('https://', '');
+
 
 export default defineConfig({
+    server: {
+        host,
+        hmr: {host},
+        https: {
+            key: fs.readFileSync(`/home/grzesiek/configs/ssl/CERTS-Storage/` + host + `.key`),
+            cert: fs.readFileSync(`/home/grzesiek/configs/ssl/CERTS-Storage/` + host + `.crt`),
+        },
+        watch: {
+            usePolling: true,
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [
+                ...refreshPaths
+            ],
         }),
     ],
 });
