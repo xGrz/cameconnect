5fe84bc8ebe1eb0ca0b98cd48d9e1fb77f0b9b69
xGrz <grzesiek.byskiniewicz@gmail.com>
2024-12-04 20:23:38 +0100

    Vite config fix for deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,14 @@
 import {defineConfig} from 'vite';
-import laravel, {refreshPaths} from 'laravel-vite-plugin';
-import fs from 'fs';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const host = `${process.env.APP_URL}`
-    .replace('http://', '')
-    .replace('https://', '');
-
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        host,
-        hmr: {host},
-        https: {
-            key: fs.readFileSync(`/home/grzesiek/configs/ssl/CERTS-Storage/` + host + `.key`),
-            cert: fs.readFileSync(`/home/grzesiek/configs/ssl/CERTS-Storage/` + host + `.crt`),
-        },
-        watch: {
-            usePolling: true,
-        }
-    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: [
-                ...refreshPaths
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/filament/player/theme.css',
+                'resources/css/filament/admin/theme.css'
             ],
         }),
     ],
