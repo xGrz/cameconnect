cc2cfb8d12145b4a42de1bc06834c78e6feb5f6f
xGrz <grzesiek.byskiniewicz@gmail.com>
2024-12-04 20:33:51 +0100

    Force

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'came-connect-overlay';
+    return 'came-connect-api';
     return \App\Services\ConnectService::make(\App\Models\User::first())->sendCommand(98101, 2);
 });
 
