418032b3b3c74c7bab72be577a69fcd2eeeeb67c
xGrz <grzesiek.byskiniewicz@gmail.com>
2024-12-19 19:48:27 +0100

    Interface ConnectUser extended with sites relationship

--- app/Interfaces/ConnectUser.php
+++ app/Interfaces/ConnectUser.php
@@ -2,10 +2,13 @@
 
 namespace App\Interfaces;
 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 interface ConnectUser
 {
     public function getConnectBearerToken(): ?string;
 
     public function getConnectUsername(): ?string;
 
+    public function sites(): BelongsToMany;
 }
